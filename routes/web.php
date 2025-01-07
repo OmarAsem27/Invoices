8b7e09616940ffebc7941d1623ef79adbe7831e3
OmarAsem27 <omarasem6215@gmail.com>
2025-01-07 12:20:04 +0200

    install laravel/ui for authentication

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/{page}', 'AdminController@index');
+Auth::routes();
+// Auth::routes(['register' => false]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Route::get('/{page}', [AdminController::class, 'index']);
