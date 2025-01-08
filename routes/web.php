68fd770ea69a569f3ca839acab912537c29d3d7a
OmarAsem27 <omarasem6215@gmail.com>
2025-01-09 00:43:30 +0200

    implement Invoice model, controller and view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\InvoiceController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -13,4 +14,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::resource('invoices', InvoiceController::class);
+
 Route::get('/{page}', [AdminController::class, 'index']);
+
