49a959e03be5d9aa754d55ed9ef85b6424528e5e
OmarAsem27 <omarasem6215@gmail.com>
2025-01-27 14:50:06 +0200

    feat(dashboard): integrate ChartJS and add invoice visualization
    - Install laravel-chartjs package
    - Add invoice chart to homepage dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Invoice;
+use IcehouseVentures\LaravelChartjs\Facades\Chartjs;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,10 +25,61 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $totalAmount = Invoice::sum('amount_collection');
-        $totalUnpaid = Invoice::where('value_status', 2)->sum('amount_collection');
-        $totalPaid = Invoice::where('value_status', 1)->sum('amount_collection');
-        $totalPartiallyPaid = Invoice::where('value_status', 3)->sum('amount_collection');
-        return view('home');
+        $totalCount = Invoice::count();
+        $countUnpaid = Invoice::where('value_status', 2)->count();
+        $countPaid = Invoice::where('value_status', 1)->count();
+        $countPartiallyPaid = Invoice::where('value_status', 3)->count();
+
+
+        if ($countUnpaid == 0) {
+            $percentageOfInvoices2 = 0;
+        } else {
+            $percentageOfInvoices2 = $countUnpaid / $totalCount * 100;
+        }
+
+        if ($countPaid == 0) {
+            $percentageOfInvoices1 = 0;
+        } else {
+            $percentageOfInvoices1 = $countPaid / $totalCount * 100;
+        }
+
+        if ($countPartiallyPaid == 0) {
+            $percentageOfInvoices3 = 0;
+        } else {
+            $percentageOfInvoices3 = $countPartiallyPaid / $totalCount * 100;
+        }
+
+
+        $chart = Chartjs::build()
+            ->name('barChartTest')
+            ->type('bar')
+            ->size(['width' => 350, 'height' => 150])
+            ->labels(['الفواتير الغير المدفوعة', 'الفواتير المدفوعة', 'الفواتير المدفوعة جزئيا'])
+            ->datasets([
+                [
+                    "label" => "الفواتير الغير المدفوعة",
+                    'backgroundColor' => ['#ec5858'],
+                    'data' => [$percentageOfInvoices2]
+                ],
+                [
+                    "label" => "الفواتير المدفوعة",
+                    'backgroundColor' => ['#81b214'],
+                    'data' => [$percentageOfInvoices1]
+                ],
+                [
+                    "label" => "الفواتير المدفوعة جزئيا",
+                    'backgroundColor' => ['#ff9642'],
+                    'data' => [$percentageOfInvoices3]
+                ],
+            ])
+            ->options([
+                "scales" => [
+                    "y" => [
+                        "beginAtZero" => true
+                    ]
+                ]
+            ]);
+
+        return view('home', compact('chart'));
     }
 }
